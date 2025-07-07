54d55423e68587bd01c3b7ea18c61ff4a051e155
skorykov-ivan <skorykov.ivan@gmail.com>
2025-07-08 00:08:09 +0300

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -1,33 +1,31 @@
-select count(*) customers_count
+select count(*) as customers_count
 from customers;
-
-
 
 --5.1. top_10_total_income.csv. Десятка лучших продавцов.
 select 
-	concat(e.first_name, ' ', e.last_name) as seller,
-	count(s.quantity) as operations,
-	floor(sum(s.quantity * p.price)) as income
-from employees e
+concat(e.first_name, ' ', e.last_name) as seller, 
+count(s.quantity) as operations,
+floor(sum(s.quantity * p.price)) as income
+from employees as e
 	
-left join sales s on e.employee_id = s.sales_person_id
-left join products p on s.product_id = p.product_id
+left join sales as s on e.employee_id = s.sales_person_id
+left join products as p on s.product_id = p.product_id
 	
 group by e.first_name, e.last_name
 order by income desc nulls last limit 10;
 
 --5.2. lowest_average_income.csv.
 with avg_inc_saller as
-		(select 
+		(select
 			concat(e.first_name, ' ', e.last_name) as seller,
 			floor(sum(s.quantity * p.price) / count(s.quantity)) as average_income,
 			round(avg(round(sum(s.quantity * p.price) / count(s.quantity),0)) over(), 0) as avg_all_income
-		from employees e
+		from employees as e
 		
-		left join sales s
+		left join sales as s
 		on e.employee_id = s.sales_person_id
 		
-		left join products p
+		left join products as p
 		on s.product_id = p.product_id
 		
 		group by e.first_name, e.last_name
@@ -41,14 +39,14 @@ where average_income < avg_all_income;-
 
 
 --5.3.day_of_the_week_income.csv.
-select 
+select
 	concat(e.first_name, ' ', e.last_name) as seller,
 	lower(trim(to_char(s.sale_date, 'Day'))) as day_of_week,
 	floor(sum(s.quantity * p.price)) as income
-from employees e
+from employees as e
 		
-left join sales s on e.employee_id = s.sales_person_id
-left join products p on s.product_id = p.product_id
+left join sales as s on e.employee_id = s.sales_person_id
+left join products as p on s.product_id = p.product_id
 		
 group by e.first_name, e.last_name, day_of_week
 having floor(sum(s.quantity * p.price)) is not null
@@ -65,21 +63,21 @@ order by case
 
               
 --6.1. age_groups.csv с возрастными группами покупателей
-select 
+select
 	'16-25' as age_category,
 	count(age) as age_count
 FROM customers
 where age between 16 and 25
 group by age_category
 union
-select 
+select
 	'26-40' as age_category,
 	count(age)
 FROM customers
 where age between 26 and 40
 group by age_category
 union 
-select 
+select
 	'40+' as age_category,
 	count(age)
 FROM customers
@@ -89,13 +87,12 @@ order by age_category;
 
 
 --6.2. customers_by_month.csv с количеством покупателей и выручкой по месяцам
-select 
+select
 	to_char(s.sale_date, 'YYYY-MM') as selling_month,
 	count(distinct s.customer_id) as total_customers,
 	floor(sum(s.quantity*p.price)) as income
-from sales s
-left join products p
-on p.product_id = s.product_id
+from sales as s
+left join products as p on p.product_id = s.product_id
 group by selling_month
 order by selling_month;
 
@@ -107,19 +104,17 @@ with tbl_answ as (
 				concat(c.first_name, ' ', c.last_name) as customer,
 				s.sale_date,
 				concat(e.first_name, ' ', e.last_name) as seller
-			from customers c
-			left join sales s
-			on c.customer_id = s.customer_id
-			left join employees e
-			on e.employee_id = s.sales_person_id
-			left join products p
-			on p.product_id = s.product_id
+			from customers as c
+			
+			left join sales as s on c.customer_id = s.customer_id
+			left join employees as e on e.employee_id = s.sales_person_id
+			left join products as p on p.product_id = s.product_id
 			
 			where s.sale_date is NOT null
 				  and p.price = 0
 			order by s.customer_id
 				  )
-select 
+select
 	customer,
 	sale_date,
 	seller
